708c5026369b4bf0a6df638c67ffbd3b8a43d738
Kierra M <k.merriman32@hotmail.com>
2019-08-08 20:52:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:adding_matz] = "yukihiro_matsumoto".unshift
+  programmer_hash[:adding_matz] = "yukihiro_matsumoto"
   puts programmer_hash  
 end
 
