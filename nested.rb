4962a3a9023309e17d436c3b00baee9e92e1b1a7
Kierra M <k.merriman32@hotmail.com>
2019-08-08 20:57:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:adding_matz] = {"yukihiro_matsumoto", known_for => "ruby", languages => ["LISP", 
-  "C"]}
+  programmer_hash[:adding_matz] = {"yukihiro_matsumoto", known_for => "ruby", languages => {["LISP", 
+  "C"]}}
  
   puts programmer_hash  
 end
